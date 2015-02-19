11c375217b0b7fa372cab13e052bde64f87b4e66
sdhawan21 <suhail.dhawan@gmail.com>
2015-02-19 18:25:12 +0100

    complete marginalisation and stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gaussian_process
 Notes, scripts and tests on gaussian processes
+
+Script now runs to do a complete marginalisation over the possible hyperparameters, using emcee and  about 10 walkers 
+
+
--- src/sample.py
+++ src/sample.py
@@ -1,7 +1,10 @@
 """
 George, sampling and marginalization for the hyperparameters
 
-Uses emcee to co
+Uses emcee to sample the posterior distribution and obtain a set of realisations 
+
+--> this approach allows you to take into account the uncertainties in the hyperparameters, which may not be well constrained by the data, which could be the case here. 
+
 """
 import george
 import numpy as np
@@ -22,7 +25,7 @@ def main():
     lc=np.loadtxt('../files/SN13689_data.dat')
     ph=lc[:,0]; mag=lc[:,1]; magerr=lc[:,2]
     magerr/=max(mag);mag=mag/max(mag)
-
+    
     gp.compute(ph, magerr)
 
     t=np.linspace(ph.min(), ph.max(), 500)
@@ -30,13 +33,15 @@ def main():
     def lnprob(p):
         if np.any((-10 > p) + (p > 10)):
             return -np.inf
-        lnprior=0.0
+        lnprior=float(sys.argv[2])
         kernel.pars=np.exp(p)
         return lnprior+ gp.lnlikelihood(mag, quiet=True)
-
+    #setup the sampler
     nwalkers, ndim = 10, len(kernel)
     sampler=emcee.EnsembleSampler(nwalkers, ndim, lnprob)
    
+
+    #initialise the walkers
     p0= [np.log(kernel.pars) + 1e-4 * np.random.randn(ndim) 	for i in range(nwalkers)]
     print "Running burn-in"
     st=time()
@@ -47,15 +52,24 @@ def main():
 
     
     sampler.run_mcmc(p0, 2000)
-    
+    prod=time()
+    print 'it took', prod-st, 'seconds'    
+    param_arr=[]
     for i in range(50):
         w = np.random.randint (sampler.chain.shape[0])
         n = np.random.randint (2000, sampler.chain.shape[1])
 
         gp.kernel.pars  = np.exp(sampler.chain[w, n])
+	#param_arr.append(gp.kernel.value)
 	plt.errorbar(ph, mag, magerr, fmt='ro')
         plt.plot(t, gp.sample_conditional(mag,t), "k", alpha=0.3)
+    
+    print 'The kernel parameter is:', gp.kernel.value
+    plt.savefig('../img/gaussfit_margin_'+sys.argv[1]+'_'+sys.argv[2]+'.png')
     plt.show()
+	
 
-
-main()
+if len(sys.argv) == 3:
+	main()
+else:
+	print "Usage: python", sys.argv[0], '<input parameter> <lnprior>'
